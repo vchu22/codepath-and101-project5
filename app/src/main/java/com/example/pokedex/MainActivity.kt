3fe01813be0707bfe29e70aec7d66c3c7f67ac6d
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-03-25 12:57:52 -0400

    finished displaying first pokemon info

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -12,25 +12,36 @@ import androidx.core.view.WindowInsetsCompat
 import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.example.pokedex.databinding.ActivityMainBinding
 import okhttp3.Headers
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
-    var species = ""
-    var type = ""
-    var pokemonImageURL = ""
-    val button = findViewById<Button>(R.id.buttonNext)
-    val textSpecies = findViewById<TextView>(R.id.textSpecies)
-    val imageView = findViewById<ImageView>(R.id.imageView)
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var imagePokemon: ImageView
+    private lateinit var textSpecies: TextView
+    private lateinit var textId : TextView
+    private lateinit var buttonNext: Button
 
+    var species = ""
+    var id = ""
+    var pokemonImageURL = ""
+
+//    val imageView = findViewById<ImageView>(R.id.imageView)
+//    val button = findViewById<Button>(R.id.buttonNext)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        textSpecies = binding.textSpecies
+        textId = binding.textId
+        imagePokemon = binding.imagePokemon
         getPokemon("pikachu")
     }
 
@@ -40,8 +51,13 @@ class MainActivity : AppCompatActivity() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
                 Log.d("Pokemon", "response successful$json")
                 species = json.jsonObject.getString("name")
-                Log.d("Pokemon Name", species)
-                textSpecies.text = species
+                textSpecies.text = species.capitalize(Locale.ROOT)
+
+                id = json.jsonObject.getInt("id").toString()
+                textId.text = id
+
+                pokemonImageURL = json.jsonObject.getJSONObject("sprites").getString("front_default")
+                loadPokemonImage(pokemonImageURL)
             }
 
             override fun onFailure(
@@ -50,8 +66,15 @@ class MainActivity : AppCompatActivity() {
                 errorResponse: String,
                 throwable: Throwable?
             ) {
-//                Log.d("Pokemon Error", errorResponse)
+                Log.d("Pokemon Error", errorResponse)
             }
         }]
+    }
+
+    private fun loadPokemonImage(imageURL: String) {
+        Glide.with(this)
+            .load(imageURL)
+            .fitCenter()
+            .into(imagePokemon)
     }
 }
