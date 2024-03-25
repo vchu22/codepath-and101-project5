5e678f7bcde36e2f6098bd11387a6090ef14eb0e
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-03-24 23:59:23 -0400

    implementing AsyncHttpClient

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.pokedex
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
+    var species = ""
+    var type = ""
+    var pokemonImageURL = ""
+    val button = findViewById<Button>(R.id.buttonNext)
+    val textSpecies = findViewById<TextView>(R.id.textSpecies)
+    val imageView = findViewById<ImageView>(R.id.imageView)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +31,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        getPokemon("pikachu")
+    }
+
+    private fun getPokemon(s: String) {
+        val client = AsyncHttpClient()
+        client["https://pokeapi.co/api/v2/pokemon/$s", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                Log.d("Pokemon", "response successful$json")
+                species = json.jsonObject.getString("name")
+                Log.d("Pokemon Name", species)
+                textSpecies.text = species
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+//                Log.d("Pokemon Error", errorResponse)
+            }
+        }]
     }
 }
